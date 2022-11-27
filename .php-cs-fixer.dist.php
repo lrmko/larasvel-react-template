b3054a219332b9ad40208b675c4aa1c1375831dd
lrmko <mpidjolrmko@gmail.com>
2022-11-27 16:22:23 +0900

    first commit

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR2' => true,
+        'blank_line_after_opening_tag' => false,
+        'linebreak_after_opening_tag' => false,
+        'declare_strict_types' => false,
+        'phpdoc_types_order' => [
+            'null_adjustment' => 'always_last',
+            'sort_algorithm' => 'none',
+        ],
+        'no_superfluous_phpdoc_tags' => false,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'php_unit_test_case_static_method_calls' => [
+            'call_type' => 'this'
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+        'not_operator_with_successor_space' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+        ->exclude([ // 除外ファイル
+              'vendor'
+        ])
+        ->in(__DIR__)
+    )
+;
